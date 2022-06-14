35e2030eaf57f77705716682e8d18bc0670e0850
Lê Hồng Minh <637942@sv.vnua.edu.vn>
2022-06-14 17:18:36 +0700

    add ultis

---
+++ resources/js/utils/helper.js
@@ -0,0 +1,39 @@
+import {VALIDATE_EMAIL_REGEX, VALIDATE_PHONE_REGEX_RULE} from "./constants";
+import moment from 'moment'
+
+export const isValidEmail = (email) => {
+    let result =  false
+    if (email && typeof email === 'string') {
+        const regex = RegExp(VALIDATE_EMAIL_REGEX);
+        result = regex.test(email.trim())
+    }
+    return result
+}
+
+export const isValidPhone = (phone) => {
+    let result = false
+
+    if (phone && typeof phone === 'string') {
+        let trimPhone = phone.trim()
+
+        if (trimPhone) {
+            const regexRule = RegExp(VALIDATE_PHONE_REGEX_RULE);
+
+            let ruleMatchs = trimPhone.match(regexRule);
+
+            if (ruleMatchs && ruleMatchs.length > 0) {
+                result = (ruleMatchs[0] === trimPhone)
+            }
+        }
+    }
+
+    return result
+}
+
+export const formatDate = (date) => {
+    return moment(date).format('HH:mm DD/MM/YYYY')
+}
+
+export const formatTime = (date) => {
+    return moment(date).lang('vi').fromNow();
+}
